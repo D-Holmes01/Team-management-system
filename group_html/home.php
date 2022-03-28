956551cb4c01bcf90964f4b2221aeb69bffb309b
JJames434 <100864052+JJames434@users.noreply.github.com>
2022-03-28 11:27:20 +0100

    Update home.php

--- group_html/home.php
+++ group_html/home.php
@@ -26,5 +26,118 @@ if (!isset($_SESSION['loggedin'])) {
 			<h2>Home Page</h2>
 			<p>Welcome back, <?=$_SESSION['name']?>!</p>
 		</div>
+		<div class="forum">
+  <p> Message Board </p>
+<div>
+<img class="profile" src="img/profile1.jpg" width="50"/>
+<div class="comment-box">
+
+  <?php
+//create_cat.php
+include 'connect.php';
+
+         
+echo '<tr>';
+    echo '<td class="leftpart">';
+        echo '<h3><a href="category.php?id=">Category name</a></h3> Category description goes here';
+    echo '</td>';
+    echo '<td class="rightpart">';                
+            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
+    echo '</td>';
+echo '</tr>';
+
+?>
+
+<form method="post" action="">
+    Category name: <input type="text" name="cat_name" />
+    Category description: <textarea name="cat_description" /></textarea>
+    <input type="submit" value="Add category" />
+ </form>
+
+
+
+ <?php
+        
+
+$cat_ID = filter_has_var(INPUT_GET, 'cat_ID') ? $_GET['cat_ID'] : null;
+$cat_Name = filter_has_var(INPUT_GET, 'cat_Name') ? $_GET['cat_Name'] : null;
+$cat_Description = filter_has_var(INPUT_GET, 'cat_description') ? $_GET['cat_description'] : null;
+
+ echo"$cat_Name";
+ echo"$cat_Description";
+
+$sql = "SELECT
+            cat_id,
+            cat_name,
+            cat_description,
+        FROM
+            categories";
+ 
+$result = mysql_query($sql);
+ 
+if(!$result)
+{
+    echo 'The categories could not be displayed, please try again later.';
+}
+else
+{
+    if(mysql_num_rows($result) == 0)
+    {
+        echo 'No categories defined yet.';
+    }
+    else
+    {
+        //prepare the table
+        echo '<table border="1">
+              <tr>
+                <th>Category</th>
+                <th>Last topic</th>
+              </tr>'; 
+             
+        while($row = mysql_fetch_assoc($result))
+        {               
+            echo '<tr>';
+                echo '<td class="leftpart">';
+                    echo '<h3><a href="category.php?id">' . $row['cat_name'] . '</a></h3>' . $row['cat_description'];
+                echo '</td>';
+                echo '<td class="rightpart">';
+                            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
+                echo '</td>';
+            echo '</tr>';
+        }
+    }
+}
+ 
+
+?>
+
+
+</div>
+</div>
+<div>
+<img class="profile" src="img/profile2.jpg" width="50"/>
+    <div class="comment-box"></div>
+</div>
+<div>
+<img class="profile" src="img/profile3.jpg" width="50"/>
+    <div class="comment-box"></div>
+</div>
+<div>
+<img class="profile" src="img/profile1.jpg" width="50"/>
+    <div class="comment-box"></div>
+</div>
+<div>
+  <div class="load-more">
+    <span class="indicator">Load more..</span>
+    </div>
+  </div>
+  </div>
+
+<br><br><br><br>
+ <footer>
+    <div class="footer-basic">
+            <p class="copyright"> © A rugby club prototype</p>
+        </footer>
+    </div>
 	</body>
 </html>
