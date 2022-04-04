ba5966da5155140f20a4b93fc933ff8b1029be1c
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-04 14:18:40 +0100

    Add files via upload

---
+++ group_html/connect.php
@@ -0,0 +1,44 @@
+<?php 
+//Describe function, variables and parameters
+
+/*//creating the getConnection function, linking the website/php to the database.
+function getConnection() {
+try {
+$connection = new PDO("mysql:host=localhost;dbname=unn_w19003579",
+  "unn_w18018436", "john1998");
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $connection;
+} catch (Exception $e) {
+throw new Exception("Connection error ". $e->getMessage(), 0, $e);
+}
+}/*
+/*
+function validate_logon(){
+
+	
+	return array ($input, $errors);
+}
+
+function show_errors(....)[
+
+}
+
+function functionName(parameters...){
+
+}*/
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'unn_w19003579';
+
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+?>
+
+
+
