f6321ff62e40814561251f5ce5a890cde22d483d
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-04 14:22:49 +0100

    Update profile.php

--- group_html/profile.php
+++ group_html/profile.php
@@ -9,17 +9,56 @@ if (!isset($_SESSION['loggedin'])) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'unn_w19003579';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT userPassword, userEmail FROM user WHERE userId = ?');
 // In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
+$stmt->bind_param('i', $_SESSION['userID']);
 $stmt->execute();
 $stmt->bind_result($password, $email);
 $stmt->fetch();
 $stmt->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td>Username:</td>
+						<td><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$_SESSION['email']?></td>
+					</tr>
+					<tr>
+						<td>Role:</td>
+						<td><?=$_SESSION['userRole']?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
