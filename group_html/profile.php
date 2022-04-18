db37541da42934a650dd3fee0ee23d8754cd90c7
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-18 21:42:59 +0100

    Update profile.php

--- group_html/profile.php
+++ group_html/profile.php
@@ -7,8 +7,8 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_USER = 'unn_w19003579';
+$DATABASE_PASS = 'Group123.';
 $DATABASE_NAME = 'unn_w19003579';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
