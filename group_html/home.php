11d7b11c70a89bd316f6ff617a878508c7f2344b
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-26 03:08:06 +0100

    Update home.php

--- group_html/home.php
+++ group_html/home.php
@@ -1,26 +1,27 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-   header('Location: index.html');
-   exit;
-}
+//call function which will connect to database and send to login if no one is logged in.
+include "function.php";
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
+   <!-- web page setup, setting charset, title and stylesheets -->
    <meta charset="utf-8">
-   <title>Home Page</title>
+   <title>Admin</title>
    <link href="style.css" rel="stylesheet" type="text/css">
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/all.css">
 </head>
+<!-- body of webpage, class loggedin so that for different css -->
 
 <body class="loggedin">
+   <!-- nav bar -->
    <nav class="navtop">
       <div>
+         <!-- Nav title and links, admin link hidden due to being the present page -->
          <h1>Website Title</h1>
+         <!-- Show admin link for users with admin priveldges-->
          <?php if ($_SESSION['userRole'] == 3 || $_SESSION['userRole'] == 4 || $_SESSION['userRole'] == 5) {
             echo '<a href="admin.php"><i class="fa-solid fa-screwdriver-wrench"></i>Admin</a>';
          }
@@ -29,8 +30,10 @@ if (!isset($_SESSION['loggedin'])) {
          <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
       </div>
    </nav>
+   <!-- page content -->
    <div class="content">
       <h2>Home Page</h2>
+      <!-- Show users name -->
       <p>Welcome back, <?= $_SESSION['name'] ?>!</p>
    </div>
    <div class="forum">
