55c64dd83e375b9f8eb2bb291589d486423e7360
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-26 03:00:31 +0100

    Update deleteUser.php

--- group_html/deleteUser.php
+++ group_html/deleteUser.php
@@ -1,35 +1,26 @@
 <?php
-session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'unn_w19003579';
-
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+//call function which will connect to database and send to login if no one is logged in.
+include "function.php";
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['user']) ) {
-	// Could not get the data that should have been sent.
-    exit('Please fill both the email and password fields!');
+if (!isset($_POST['user'])) {
+	// Could not get the data
+	exit('Please fill both the email and password fields!');
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare("Delete from user WHERE userID =".$_POST['userID']." and userRole <> 4;")) {
+if ($stmt = $con->prepare("Delete from user WHERE userID =" . $_POST['userID'] . " and userRole <> 4;")) {
+	//Execute SQL
 	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-    header('Location: home.php');
-    } else {
-        // Incorrect email
-		echo "<script> alert('Failed to log in: Delete from user WHERE userID =".$_POST['userID']." and userRole <> 4;');
-		window.location.href='index.html';
+	//Return to the homepage
+	echo "<script> alert('User deleted');
+		window.location.href='home.php';
 		</script>";
-    }
-	$stmt->close();
-?>
+} else {
+	// Incorrect email
+	echo "<script> alert('Failed to delete user');
+		window.location.href='home.php';
+		</script>";
+}
+//close statement
+$stmt->close();
