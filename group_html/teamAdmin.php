bb0bc8293f8492187e5d8d884af522e37d7347a0
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-26 03:13:40 +0100

    Update teamAdmin.php

--- group_html/teamAdmin.php
+++ group_html/teamAdmin.php
@@ -1,57 +1,43 @@
 <?php
+//call function which will connect to database and send to login if no one is logged in.
 include "function.php";
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-    header('Location: index.html');
-    exit;
-}
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'unn_w19003579';
-$DATABASE_PASS = 'Group123.';
-$DATABASE_NAME = 'unn_w19003579';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT userPassword, userEmail FROM user WHERE userId = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['squadID']);
-$stmt->execute();
-$stmt->bind_result($password, $email);
-$stmt->fetch();
-$stmt->close();
-
-$result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['teamID'] . ';');
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
+    <!-- web page setup, setting charset, title and stylesheets -->
     <meta charset="utf-8">
-    <title>Profile Page</title>
+    <title>Admin</title>
     <link href="style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/all.css">
-    <script type="text/javascript" src="assessmentJS.js"></script>
-    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
 </head>
+<!-- body of webpage, class loggedin so that for different css -->
 
 <body class="loggedin">
+    <!-- nav bar -->
     <nav class="navtop">
         <div>
-            <h1>Website Title</h1>
+            <!-- Nav title and links, admin link hidden due to being the present page -->
+            <h1>Admin</h1>
+            <a href="admin.php"><i class="fa-solid fa-screwdriver-wrench"></i>Admin</a>
             <a href="home.php"><i class="fa-solid fa-house"></i>Home</a>
             <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
             <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
         </div>
     </nav>
+    <!-- page content -->
     <div class="content">
+        <!-- team selection form-->
         <div id="teamSelect">
             <form action="assignTeam.php" method="post">
-                <p>Users:</p>
+                <h2>Add player to team</h2>
+                <h3>Users:</h3>
+                <!-- select is populated by database users-->
                 <select name="users" id="users">
-                    <?php 
+                    <?php $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['teamID'] . ';');
+
                     while (
                         $ri = mysqli_fetch_array($result)
                     ) {
@@ -63,10 +49,11 @@ $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['t
                     <?php
                     }
                     ?>
-                </select>
-                <select name="teams" id="teams">
-                    <p>Teams</p>
 
+                </select>
+                <h3>Teams</h3>
+                <!-- select is populated by database teams -->
+                <select name="teams" id="teams">
                     <?php
                     $result = $con->query("SELECT squad.squadID, squad.squadName from teamSquad LEFT JOIN squad on squad.squadID = teamSquad.squadID where teamSquad.teamID = " . $_SESSION['teamID'] . ";");
                     while (
@@ -84,12 +71,14 @@ $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['t
                 <input type="submit" id="assignTeam"></input>
             </form>
         </div>
+        <!-- captaincy selection div -->
         <div id="captainSelect">
+            <h2>Assign captaincy</h2>
+            <!-- captaincy form, on submit this will update the team captaincy to the select user-->
             <form action="assignCaptain.php" method="post">
-                
+                <h3>Teams:</h3>
                 <select name="teams" id="teams">
-                    <p>Teams</p>
-
+                    <!-- fill select with teams from database-->
                     <?php
                     $result = $con->query("SELECT squad.squadID, squad.squadName from teamSquad LEFT JOIN squad on squad.squadID = teamSquad.squadID where teamSquad.teamID = " . $_SESSION['teamID'] . ";");
                     while (
@@ -104,10 +93,11 @@ $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['t
                     }
                     ?>
                 </select>
-                <p>Users:</p>
+                <h3>Players:</h3>
+                <!-- fill select with users from db -->
                 <select name="users" id="users">
-                    
-                    <?php 
+
+                    <?php
                     echo 'SELECT * from user where user.userTeam = ' . $_SESSION['teamID'] . ' AND user.userRole < 3;';
                     $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['teamID'] . ' AND user.userRole < 3;');
                     while (
@@ -125,11 +115,15 @@ $result = $con->query('SELECT * from user where user.userTeam = ' . $_SESSION['t
                 <input type="submit" id="assignCaptain"></input>
             </form>
         </div>
+        <!-- div for new teams -->
         <div id="newTeam">
+            <h2>Create new team</h2>
+            <!-- form which on submit will create a new squad in the database with the enter name-->
             <form action="createTeam.php" method="post">
-                <input type="text" name = "name">
-            <input type="submit" id="createTeam.php"></input>
+                <h3>New team name:</h3>
 
+                <input type="text" name="name">
+                <input type="submit" id="createTeam.php"></input>
             </form>
         </div>
     </div>
