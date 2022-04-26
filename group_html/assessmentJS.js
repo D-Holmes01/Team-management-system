85c519b451d71239d3c7c3b2bb0845562192f114
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-26 02:53:36 +0100

    Delete assessmentJS.js

--- group_html/assessmentJS.js
+++
@@ -1,9 +0,0 @@
-//onpage load functions
-window.onload = function () {
-    function assignRole(){
-            //setting the file for the html to be retrieved from
-            $roleId = document.getElementById("assignRole");
-            const addRole = 'function.php?role'; 
-    }
-
-}
