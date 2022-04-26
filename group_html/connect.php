79c750341ef8c97d4e2a90847c6c9d913d5d2b0e
JJames434 <100864052+JJames434@users.noreply.github.com>
2022-04-26 03:29:19 +0100

    Add files via upload

--- group_html/connect.php
+++ group_html/connect.php
@@ -1,4 +1,6 @@
-<?php
+
+   
+<?php 
 
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'unn_w19003579';
@@ -7,7 +9,10 @@ $DATABASE_NAME = 'unn_w19003579';
 
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
+if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+?>
+
+
