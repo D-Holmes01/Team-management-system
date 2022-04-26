bf737f13436ceed8459b4bfec38cc5db5d17307a
D-Holmes01 <100799660+D-Holmes01@users.noreply.github.com>
2022-04-26 02:58:04 +0100

    Update createTeam.php

--- group_html/createTeam.php
+++ group_html/createTeam.php
@@ -1,42 +1,34 @@
 <?php
-session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'unn_w19003579';
-$DATABASE_PASS = 'Group123.';
-$DATABASE_NAME = 'unn_w19003579';
+//call function which will connect to database and send to login if no one is logged in.
+include "function.php";
 
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit("Insert into squad(squadName) values(".$_POST['name'] .");INSERT INTO teamsquad (squadID, teamID) values ((SELECT squadID from squad where squadName = '".$_POST['name']."'), ".$_SESSION['teamID'].");");
-}
-
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['name']) ) {
-	// Could not get the data that should have been sent.
-	echo $_POST['name'];
-    exit('Please fill both the email and password fields!');
+//Check the values are passed
+if (!isset($_POST['name'])) {
+	// Could not get the data
+	exit('Please fill the name field!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare("Insert into squad(squadName) values('".$_POST['name'] ."');")) {
+if ($stmt = $con->prepare("Insert into squad(squadName) values('" . $_POST['name'] . "');")) {
+	//Execute SQL
 	$stmt->execute();
-    } else {
-        // Incorrect email
-		echo "<script> alert('Failed to log in: incorrect details.');
-			window.location.href='index.html';
-			</script>";
-    }
-    
+} else {
+	// Incorrect email
+	echo "<script> alert('Failed to Create team');
+		window.location.href='home.php';
+		</script>";
+}
+
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare("INSERT INTO teamsquad (squadID, teamID) values ((SELECT squadID from squad where squadName = '".$_POST['name']."'), ".$_SESSION['teamID'].");")) {
+if ($stmt = $con->prepare("INSERT INTO teamsquad (squadID, teamID) values ((SELECT squadID from squad where squadName = '" . $_POST['name'] . "'), " . $_SESSION['teamID'] . ");")) {
+	//Execute SQL
 	$stmt->execute();
-    header('Location: home.php');
-    } else {
-        // Incorrect email
-		echo "<script> alert('Failed to log in: incorrect details.');
-			window.location.href='index.html';
-			</script>";
-    }
-?>
+	//Return to the homepage
+	echo "<script> alert('Team created');
+		window.location.href='home.php';
+		</script>";
+} else {
+	// Failed to assign squad ot team
+	echo "<script> alert('Failed to assign team to club');
+		window.location.href='home.php';
+		</script>";
+}
