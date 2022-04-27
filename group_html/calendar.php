987e0b3852061bb94890c6244900522ed15b785f
yousifnaser <78973010+yousifnaser@users.noreply.github.com>
2022-04-28 01:09:38 +0300

    Update calendar.php

--- group_html/calendar.php
+++ group_html/calendar.php
@@ -14,12 +14,6 @@
       if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin'] == TRUE))
       {
 
-        //link the user to the playerCalendar page if the userRole is player
-        if ($_SESSION['userRole'] == '1')
-        {
-            header("Location: http://unn-w19003579.newnumyspace.co.uk/group/playerCalendar.php");
-        }
-
         //link the user to the captainCalendar page if the userRole is captain
         if ($_SESSION['userRole'] == '2')
         {
@@ -32,10 +26,10 @@
             header("Location: http://unn-w19003579.newnumyspace.co.uk/group/adminCalendar.php");
         }
 
-        //displays an error if $_SESSION['userRole'] is none of the above options
+        //otherwise the user is linked to the playerCalendar page
         else
         {
-            echo "User Account Type cannot be identified";
+          header("Location: http://unn-w19003579.newnumyspace.co.uk/group/playerCalendar.php");
         }
 
       }
